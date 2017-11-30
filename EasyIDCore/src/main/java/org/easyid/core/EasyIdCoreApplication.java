440f31df2dec42814efd7b7b6fd946e3e441fe7e
Hongyan Song <hongyan.song@ryerson.ca>
2017-11-30 14:35:47 -0500

    Add easyid-core and easyid-ws modules.

---
+++ EasyIDCore/src/main/java/org/easyid/core/EasyIdCoreApplication.java
@@ -0,0 +1,12 @@
+package org.easyid.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyIdCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyIdCoreApplication.class, args);
+	}
+}
