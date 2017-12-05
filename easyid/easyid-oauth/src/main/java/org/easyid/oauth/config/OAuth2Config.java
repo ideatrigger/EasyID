1a4d89610fa27e524dabdb246717fdaea1d37e7d
Hongyan Song <hongyan.song@ryerson.ca>
2017-12-05 14:59:45 -0500

    Add authenticationManager to OAuth2Config in easyid-oauth module to support password grant type.

--- easyid/easyid-oauth/src/main/java/org/easyid/oauth/config/OAuth2Config.java
+++ easyid/easyid-oauth/src/main/java/org/easyid/oauth/config/OAuth2Config.java
@@ -1,20 +1,33 @@
 package org.easyid.oauth.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	/**
+	 * In order to support "password" grant type, an AuthenticationManager must be configured.
+	 */
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+		endpoints.authenticationManager(authenticationManager);
+	}
 	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
 				.withClient("service-account-1")
 				.secret("service-account-1-secret")
-				.authorizedGrantTypes("client_credentials")
+				.authorizedGrantTypes("client_credentials", "refresh_token", "password")
 				.scopes("resource-server-read", "resource-server-write")
 				.authorities("ROLE_RS_READ");
 	}
